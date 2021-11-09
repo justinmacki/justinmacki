2edda5f0ea049fc75527c8e6d145f92031279d98
Justin Macki Julhusin <75675003+justinmacki@users.noreply.github.com>
2021-11-09 21:04:27 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Justin Macki Julhusin`,
+        short_name: `Justin Macki`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/my-logo.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sass`,
